27b2bad66e203c7671ad5d6b497d4285f521c548
JoshJ-OSU <johnsjo3@onid.oregonstate.edu>
2015-07-27 14:36:11 -0700

    photoUpload.php script is now in the form of a callable function.

--- photoUpload.php
+++ photoUpload.php
@@ -1,70 +1,85 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<form action="photoUpload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
-
-</body>
-</html>
-
 <?php
+/*
+* fileUploadError(str)
+*---------------------
+* For handling errors in file uploads later
+*/
+function fileUploadError($str) {
+    echo $str;
+    return;
+}
 
-// Based off of PHP 5 File Upload tutorial by W3 Schools at http://www.w3schools.com/php/php_file_upload.asp
 
-$date = date('YmdHis');
-$target_dir = "photos/";
+/*
+* photoUpload()
+* -----------
+* For a file upload to $_FILES, checks aspects of that file, then uploads.
+* Returns URL for successful uploads or NULL if unsuccessful
+* 
+* Based off of PHP 5 File Upload tutorial by W3 Schools at http://www.w3schools.com/php/php_file_upload.asp
+* Better version may be at http://php.net/manual/en/features.file-upload.php
+*/
 
-// Append timestamp to file name
-$raw_file_name = explode(".", basename($_FILES["fileToUpload"]["name"]));
-$target_file = $target_dir . $raw_file_name[0] . $date . '.' . $raw_file_name[1];
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
+function uploadPhoto() {
+    //set NULL in cases where no file is selected
+    if (!( isset($_FILES["fileToUpload"]["name"]) )) {
+      return NULL;
+    }
+    
+    //Target Directory to save file to:
+    $target_dir = "photos/";
+
+    
+    // Append timestamp to file name
+    $date = date('YmdHis');
+    $raw_file_name = explode(".", basename($_FILES["fileToUpload"]["name"]));
+    $target_file = $target_dir . $raw_file_name[0] . $date . '.' . $raw_file_name[1];
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            $uploadOk = 1;
+        } else {
+            fileUploadError("File is not an image");
+            $uploadOk = 0;
+        }
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        fileUploadError("That file already exists");
         $uploadOk = 0;
     }
-}
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        fileUploadError("File is too large.");
+        $uploadOk = 0;
+    }
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        fileUploadError("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+        $uploadOk = 0;
+    }
 
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        return NULL;
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    // if everything is ok, try to upload file
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            return $target_file;
+        } else {
+            fileUploadError("There was an error uploading the file.");
+            return NULL;
+        }
     }
 }
 ?>
